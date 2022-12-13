4d13ffe511671aa13237c7c480bc2657bb32c20d
Shweta <shwetaghanwat5@gmail.com>
2022-12-13 20:41:26 +0530

    node js assignment 7 is added

---
+++ Assignment-7-node/src/InitialData.js
@@ -0,0 +1,44 @@
+var studentArray = [{
+    id: 1,
+    name: 'Shweta',
+    currentClass: 5,
+    division: 'A'
+},
+{
+    id: 2,
+    name: 'Payal',
+    currentClass: 12,
+    division: 'C'
+},
+{
+    id: 3,
+    name: 'Priya',
+    currentClass: 7,
+    division: 'C'
+},
+{
+    id: 4,
+    name: 'Shiv',
+    currentClass: 7,
+    division: 'A'
+},
+{
+    id: 5,
+    name: 'Umesh',
+    currentClass: 10,
+    division: 'D'
+},
+{
+    id: 6,
+    name: 'Sanket',
+    currentClass: 10,
+    division: 'A'
+},
+{
+    id: 7,
+    name: 'Pranali',
+    currentClass: 8,
+    division: 'A'
+}]
+
+module.exports = studentArray;
